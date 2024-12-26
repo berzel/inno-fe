87cf538218c0c26e950a3643ce7d35d675d11e45
Berzel Best <berzel.best@gmail.com>
2024-12-26 11:01:00 +0200

    Login page layout

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home/home.tsx")
+    index("routes/home/home.tsx"),
+    route("/login", "routes/login/login.tsx"),
+    route("/register", "routes/register/register.tsx"),
 ] satisfies RouteConfig;
---
+++ app/routes/login/login.tsx
@@ -0,0 +1,43 @@
+import {Link} from "react-router";
+
+export default function Login() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="space-y-12">
+                <div className="border-b border-opacity-10 border-[#072009] py-4">
+                    <h1 className="font-semibold text-2xl">Login</h1>
+                    <p className="text-sm mt-2 text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div className="space-y-6">
+                    <div>
+                        <label className="space-y-2 block">
+                            <div className="font-medium">Email</div>
+                            <input type="text" placeholder="you@example.com"
+                                   className="bg-transparent border border-[#072009] rounded-md p-4 min-w-[25rem]"/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label className="space-y-2 block">
+                            <div className="font-medium">Password</div>
+                            <input type="password" placeholder="********"
+                                   className="bg-transparent border border-[#072009] rounded-md p-4 min-w-[25rem]"/>
+                        </label>
+
+                        <div className="text-right mt-2">
+                            <Link to="#" className="inline-block hover:underline">
+                                Forgot password?
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button className="block w-full bg-[#072009] text-white rounded-md p-4 font-medium mt-12">
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
